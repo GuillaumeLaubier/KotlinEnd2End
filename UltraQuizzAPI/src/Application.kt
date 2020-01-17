ca5a74b7ebdefaa5a7aee0639faa4b657daece24
Guillaume Laubier <glaubier@smartadserver.com>
2020-01-17 16:42:43 +0100

    Crate Start route and first draw of score route

--- UltraQuizzAPI/src/Application.kt
+++ UltraQuizzAPI/src/Application.kt
@@ -2,6 +2,7 @@ package com.kotlinend2end.ultraquizzapi
 
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.kotlinend2end.ultraquizzapi.model.GameScore
 import com.kotlinend2end.ultraquizzapi.model.Question
 import io.ktor.application.*
 import io.ktor.response.*
@@ -10,8 +11,11 @@ import io.ktor.http.*
 import io.ktor.gson.*
 import io.ktor.features.*
 import io.ktor.client.features.auth.basic.*
+import java.io.File
 import java.io.FileReader
+import java.io.FileWriter
 import java.text.DateFormat
+import java.util.*
 import kotlin.random.Random
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
@@ -49,24 +53,55 @@ fun Application.module(testing: Boolean = false) {
     }*/
 
     routing {
+
+        /// Debug routes below
+
         get("/") {
-            println("Route / called.")
+            println("Route GET / called.")
             call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
         }
 
         get("/json/gson") {
-            println("Route /json/gson called.")
+            println("Route GET /json/gson called.")
             call.respond(mapOf("hello" to "world"))
         }
 
         get("/json/questions") {
-            println("Route /json/questions called.")
+            println("Route GET /json/questions called.")
             call.respond(questionsList)
         }
 
         get("/json/questions/random") {
-            println("Route /json/questions/random called.")
+            println("Route GET /json/questions/random called.")
             call.respond(questionsList[Random.nextInt(0, questionsList.size)])
+        }
+
+        /// Actual routes below
+
+        get("/start") {
+            println("Route GET /start called.")
+            // Select and return 5 random questions
+            call.respond(questionsList.shuffled().subList(0, 5))
+        }
+
+        post("/score") {
+            println("Route POST /score called.")
+
+            // response json formatted:
+            // {
+            //   "name": <gamer_name>,
+            //   "score" : <score_value>
+            // }
+
+
+            // put fix score for testing
+            val newScore = GameScore(Date(), "Guiguilabrute", 5)
+            val scoreJson = Gson().toJson(newScore)
+
+            val scoreFile = File("resources/scores.json")
+            scoreFile.writeText(scoreJson)
+
+            call.respond(HttpStatusCode.Accepted, "Score is stored")
         }
     }
 }
---
+++ UltraQuizzAPI/src/model/gamescore.kt
@@ -0,0 +1,5 @@
+package com.kotlinend2end.ultraquizzapi.model
+
+import java.util.*
+
+data class GameScore(val date: Date, val gamerName: String, val score: Int)
